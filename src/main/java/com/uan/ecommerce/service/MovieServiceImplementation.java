0ab41b921cbd1a128f33d6cf644134ad35800d06
Diego Lopez <74331334+AspireRB@users.noreply.github.com>
2023-02-28 00:23:32 -0500

    Movie crud service class

---
+++ src/main/java/com/uan/ecommerce/service/MovieServiceImplementation.java
@@ -0,0 +1,36 @@
+package com.uan.ecommerce.service;
+
+import com.uan.ecommerce.model.Movie;
+import com.uan.ecommerce.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class MovieServiceImplementation implements MovieService {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @Override
+    public Movie save(Movie movie) {
+        return movieRepository.save(movie);
+    }
+
+    @Override
+    public Optional<Movie> get(Integer id) {
+        return movieRepository.findById(id);
+    }
+
+    @Override
+    public void update(Movie movie) {
+        movieRepository.save(movie);
+    }
+
+    @Override
+    public void delete(Integer id) {
+        movieRepository.deleteById(id);
+    }
+
+}
