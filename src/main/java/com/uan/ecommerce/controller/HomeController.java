d96f556c85ab2938f1a3718d90e6693a982593d3
Diego Lopez <74331334+AspireRB@users.noreply.github.com>
2023-05-02 00:47:07 -0500

    Add the movies to the shopping cart and show in the cart

--- src/main/java/com/uan/ecommerce/controller/HomeController.java
+++ src/main/java/com/uan/ecommerce/controller/HomeController.java
@@ -1,16 +1,19 @@
 package com.uan.ecommerce.controller;
 
+import com.uan.ecommerce.model.Detail;
 import com.uan.ecommerce.model.Movie;
+import com.uan.ecommerce.model.Order;
 import com.uan.ecommerce.service.MovieService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.sql.Struct;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -21,6 +24,10 @@ public class HomeController {
 
     @Autowired
     private MovieService movieService;
+
+    List<Detail> details = new ArrayList<Detail>();
+
+    Order order = new Order();
 
     @GetMapping("")
     public String home(Model model) {
@@ -38,5 +45,33 @@ public class HomeController {
         model.addAttribute("movie", movie);
 
         return "user/moviehome";
+    }
+
+    @PostMapping("/cart")
+    public String addCart(@RequestParam Integer id, @RequestParam Integer amount, Model model) {
+        Detail detail = new Detail();
+        Movie movie = new Movie();
+        double totalAmount = 0;
+
+        Optional<Movie> optionalMovie = movieService.get(id);
+        log.info("Add movie: {}", optionalMovie.get());
+        log.info("Amount: {}", amount);
+        movie = optionalMovie.get();
+
+        detail.setAmount(amount);
+        detail.setPrice(movie.getPrice());
+        detail.setName(movie.getName());
+        detail.setPay(movie.getPrice()*amount);
+        detail.setMovie(movie);
+
+        details.add(detail);
+
+        totalAmount = details.stream().mapToDouble(dt->dt.getPay()).sum();
+
+        order.setPay(totalAmount);
+        model.addAttribute("cart", details);
+        model.addAttribute("order", order);
+
+        return "user/cart";
     }
 }
