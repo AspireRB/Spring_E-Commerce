4cc4a928218ecc48f64195e15d013a75ef7a5d65
Diego Lopez <74331334+AspireRB@users.noreply.github.com>
2023-05-01 20:48:06 -0500

    Implement the upload and display image functionalities

--- src/main/java/com/uan/ecommerce/controller/AdministratorController.java
+++ src/main/java/com/uan/ecommerce/controller/AdministratorController.java
@@ -1,15 +1,27 @@
 package com.uan.ecommerce.controller;
 
+import com.uan.ecommerce.model.Movie;
+import com.uan.ecommerce.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/administrator")
 public class AdministratorController {
 
+    @Autowired
+    private MovieService movieService;
+
     @GetMapping("")
-    public String home() {
+    public String home(Model model) {
+        List<Movie> movies = movieService.findAll();
+        model.addAttribute("movies", movies);
+
         return "administrator/home";
     }
 
--- src/main/java/com/uan/ecommerce/controller/MovieController.java
+++ src/main/java/com/uan/ecommerce/controller/MovieController.java
@@ -3,16 +3,16 @@ package com.uan.ecommerce.controller;
 import com.uan.ecommerce.model.User;
 import com.uan.ecommerce.model.Movie;
 import com.uan.ecommerce.service.MovieService;
+import com.uan.ecommerce.service.UploadFileService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Optional;
 
 @Controller
@@ -23,6 +23,9 @@ public class MovieController {
 
     @Autowired
     private MovieService movieService;
+
+    @Autowired
+    private UploadFileService upload;
 
     @GetMapping("")
     public String show(Model model) {
@@ -36,10 +39,19 @@ public class MovieController {
     }
 
     @PostMapping("/save")
-    public String save(Movie movie) {
+    public String save(Movie movie, @RequestParam("img") MultipartFile file) throws IOException {
         LOGGER.info("This is the object movie {}",movie);
         User u = new User(1, "", "", "", "");
         movie.setUser(u);
+
+        //image
+        if (movie.getId()==null) { // when create a movie
+            String nameImage = upload.saveImage(file);
+            movie.setImage(nameImage);
+        } else {
+
+        }
+
         movieService.save(movie);
         return "redirect:/movies";
     }
@@ -57,13 +69,37 @@ public class MovieController {
     }
 
     @PostMapping("/update")
-    public String update(Movie movie) {
+    public String update(Movie movie, @RequestParam("img") MultipartFile file) throws IOException {
+        Movie m = new Movie();
+        m = movieService.get(movie.getId()).get();
+
+        if (file.isEmpty()) { //edit movie but i dont change image
+            movie.setImage(m.getImage());
+        } else {
+            //delete when it dont default image
+            if (!m.getImage().equals("default.jpg")) {
+                upload.deleteImage(m.getImage());
+            }
+
+            String nameImage = upload.saveImage(file);
+            movie.setImage(nameImage);
+        }
+        movie.setUser(m.getUser());
         movieService.update(movie);
         return "redirect:/movies";
     }
 
     @GetMapping("/delete/{id}")
     public String delete(@PathVariable Integer id) {
+
+        Movie m = new Movie();
+        m = movieService.get(id).get();
+
+        //delete when it dont default image
+        if (!m.getImage().equals("default.jpg")) {
+            upload.deleteImage(m.getImage());
+        }
+
         movieService.delete(id);
         return "redirect:/movies";
     }
